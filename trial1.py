f3d11a95bd3fab4cbbefd6d81ab43900cd52bac7
roshanarvind <roshanarvind001@gmail.com>
2017-07-15 18:50:28 +0530

    Initialisation

--- trial1.py
+++ trial1.py
@@ -1,8 +1,24 @@
 import re
+import csv
+
+class distributor:
+	"""docstring for distributor"""
+	dist_total = 0 
+
+	def __init__(self, distributor_id, include, exclude):
+		self.distributor_id = distributor_id
+		self.include = include
+		self.exclude = exclude
+		dist_total += 1
+		
+
+def read_file():
+	with open('cities.csv', 'rb') as csvfile:
+		return list(csv.reader(csvfile))[1:]
 
 def main():
-	print "Hello World!"
-	print "Hello World again!"
+	cities_list = read_file()
+	print cities_list
 
 if __name__ == '__main__':
 	main()
