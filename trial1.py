dc63501082049a6e9a04187a7db5ff74ec87e8ee
roshanarvind <roshanarvind001@gmail.com>
2017-07-17 23:24:52 +0530

    New Modifications

--- trial1.py
+++ trial1.py
@@ -1,14 +1,18 @@
-import re
+from sets import Set
 import csv
 
 class distributor:
 	"""docstring for distributor"""
 	dist_total = 0 
 
-	def __init__(self, distributor_id, include, exclude):
+	def __init__(self, distributor_id):
 		self.distributor_id = distributor_id
-		self.include = include
-		self.exclude = exclude
+		self.i_countries = []
+		self.i_provinces = []
+		self.i_cities = []
+		self.e_countries = []
+		self.e_provinces = []
+		self.e_cities = []
 		dist_total += 1
 		
 
@@ -18,7 +22,15 @@ def read_file():
 
 def main():
 	cities_list = read_file()
-	print cities_list
+	countries=Set([])
+	provinces=Set([])
+	cities=Set([])
+	for row in cities_list:
+		countries.add(row[5])
+		provinces.add(row[4])
+		cities.add(row[3])
+	print len(cities), len(provinces), len(countries)
+	print countries
 
 if __name__ == '__main__':
 	main()
