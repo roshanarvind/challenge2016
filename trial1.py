1a10c5eea77e0e4e4888ef0088a406ffc5339b74
roshanarvind <roshanarvind001@gmail.com>
2017-08-04 23:43:40 +0530

    15% Complete

--- trial1.py
+++ trial1.py
@@ -1,20 +1,64 @@
-from sets import Set
 import csv
 
+distributor_list=[]
+dist_total=0
+def counter():
+	dist_total+=1
+	return dist_total
+
+countries={}
+provinces={}
+cities={}
 class distributor:
 	"""docstring for distributor"""
-	dist_total = 0 
-
-	def __init__(self, distributor_id):
-		self.distributor_id = distributor_id
-		self.i_countries = []
-		self.i_provinces = []
-		self.i_cities = []
+	dist_total = 1
+	def __init__(self,co_list,ci_list,pr_list):
+		self.distributor_id = counter()
+		self.i_countries = co_list
+		self.i_provinces = pr_list
+		self.i_cities = ci_list
 		self.e_countries = []
 		self.e_provinces = []
 		self.e_cities = []
-		dist_total += 1
-		
+
+	def add_parent_link(self, parent_id):
+		#self.i_countries.append()
+		i=0
+
+def search(string, id):
+	if id==1:
+		if string in cities:
+			return cities[string]
+	if id==2:
+		if string in provinces:
+			return provinces[string]
+	if id==3:
+		if string in countries:
+			return countries[string]
+	return -1 
+def add_distributor():
+	sub_dist = int(raw_input('Is it a sub distributor? : '))
+	
+	if sub_dist:
+		parent_id = int(raw_input('Enter Parent ID : '))
+	else:
+		parent_id = -1
+	co_list = []
+	ci_list = []
+	pr_list = []
+	n_cities,n_provinces,n_countries = map(int, raw_input('Enter number of cities, provinces and countries you want to include : ').split(','))
+	for i in xrange(n_cities):
+		ci_list.append(search(raw_input(),1))
+	for i in xrange(n_provinces):
+		pr_list.append(search(raw_input(),2))
+	for i in xrange(n_countries):
+		co_list.append(search(raw_input(),3))
+
+	distributor_list.append(distributor(co_list,ci_list,pr_list))
+
+def show_distributor():
+	for i in distributor_list:
+		print i.i_cities,i.distributor_id
 
 def read_file():
 	with open('cities.csv', 'rb') as csvfile:
@@ -22,9 +66,7 @@ def read_file():
 
 def main():
 	cities_list = read_file()
-	countries={}
-	provinces={}
-	cities={}
+
 	a,b,c = 0,0,0
 	for row in cities_list:
 		if row[5] not in countries:
@@ -37,5 +79,14 @@ def main():
 			c+=1	
 			cities[row[3]]=c
 	
+	while 1:
+		option = int(raw_input())
+		if option == 0:
+			break
+		elif option == 1:
+			add_distributor()
+		elif option == 2:
+			show_distributor()
+			
 if __name__ == '__main__':
 	main()
